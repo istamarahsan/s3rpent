c24dae694fe545cf51df6471765848b1699ec3ad
San <istamarahsan@gmail.com>
2023-07-04 10:35:20 +0700

    directional movement and WASD

--- input/turn_input.gd
+++ input/turn_input.gd
@@ -1,10 +1,16 @@
 extends Node
 
-signal left_pressed()
-signal right_pressed()
+signal left_pressed
+signal right_pressed
+signal up_pressed
+signal down_pressed
 
 func _input(event):
 	if event.is_action_pressed("ui_left"):
 		left_pressed.emit()
 	if event.is_action_pressed("ui_right"):
 		right_pressed.emit()
+	if event.is_action_pressed("ui_up"):
+		up_pressed.emit()
+	if event.is_action_pressed("ui_down"):
+		down_pressed.emit()
--- root.gd
+++ root.gd
@@ -4,11 +4,29 @@ enum UpperState {
 	Playing
 }
 
+enum InputDirection {
+	Right,
+	Up,
+	Left,
+	Down
+}
+
+var input_lookup = {
+	[InputDirection.Left, Vector2i.UP] : CybersnakeGame.TurnDirection.Left,
+	[InputDirection.Right, Vector2i.UP] : CybersnakeGame.TurnDirection.Right,
+	[InputDirection.Left, Vector2i.DOWN] : CybersnakeGame.TurnDirection.Right,
+	[InputDirection.Right, Vector2i.DOWN] : CybersnakeGame.TurnDirection.Left,
+	[InputDirection.Up, Vector2i.RIGHT] : CybersnakeGame.TurnDirection.Left,
+	[InputDirection.Down, Vector2i.RIGHT] : CybersnakeGame.TurnDirection.Right,
+	[InputDirection.Up, Vector2i.LEFT] : CybersnakeGame.TurnDirection.Right,
+	[InputDirection.Down, Vector2i.LEFT] : CybersnakeGame.TurnDirection.Left
+}
+
 @export var game_scene: PackedScene
 
-@onready var inner_game: CybersnakeGame
 @onready var game_presentation = $Presentation as Presentation
 
+var inner_game: CybersnakeGame
 var state_hooks: Array[StateHook] = []
 var upper_state: UpperState = UpperState.Playing
 var is_action_cooldown: bool = false
@@ -39,19 +57,24 @@ func _get_tree_rec(root: Node) -> Array[Node]:
 		nodes.append_array(_get_tree_rec(child))
 	return nodes
 
-func _on_turn_input_left_pressed():
+func _handle_direction_input(dir: InputDirection):
 	if is_action_cooldown:
 		return
-	is_action_cooldown = true
-	inner_game.action_turn(CybersnakeGame.TurnDirection.Left)
-	_propagate_hook_signal()
+	var translated = input_lookup.get([dir, inner_game.snake_heading], null)
+	if translated != null and translated is CybersnakeGame.TurnDirection:
+		is_action_cooldown = true
+		inner_game.action_turn(translated)
+		_propagate_hook_signal()
 
+func _propagate_hook_signal():
+	for hook in state_hooks:
+		hook.emit_signal("updated")
+
+func _on_turn_input_left_pressed():
+	_handle_direction_input(InputDirection.Left)
+	
 func _on_turn_input_right_pressed():
-	if is_action_cooldown:
-		return
-	is_action_cooldown = true
-	inner_game.action_turn(CybersnakeGame.TurnDirection.Right)
-	_propagate_hook_signal()
+	_handle_direction_input(InputDirection.Right)
 
 func _on_timer_timeout():
 	inner_game.process_timestep()
@@ -61,6 +84,8 @@ func _on_timer_timeout():
 func _on_demo_ui_request_replay():
 	_recreate_game()
 
-func _propagate_hook_signal():
-	for hook in state_hooks:
-		hook.emit_signal("updated")
+func _on_turn_input_up_pressed():
+	_handle_direction_input(InputDirection.Up)
+
+func _on_turn_input_down_pressed():
+	_handle_direction_input(InputDirection.Down)
